6f3fe07e8cab085b9c1d96b403703f3b12a514cb
AdityaSingh17 <adityarsingh17@gmail.com>
2019-11-12 15:56:53 +0530

    Added SQL program for Orders Database

--- 02.Orders.sql
+++ 02.Orders.sql
@@ -1,3 +1,4 @@
+--Create table salesman.
 create table salesman
 (
 	salesman_id number(4),
@@ -7,6 +8,7 @@ create table salesman
 	Constraint pk_salesmanid primary key(salesman_id)
 );
 
+--Create table customer.
 create table customer
 (
 	customer_id number(4),
@@ -18,6 +20,7 @@ create table customer
 	Constraint fk_salesmanid foreign key(salesman_id) references salesman(salesman_id) on delete cascade
 );
 
+--Create table orders.
 create table orders
 (
 	ord_no number(4),
@@ -30,26 +33,26 @@ create table orders
 	Constraint fk_customerid foreign key(customer_id) references customer(customer_id) on delete cascade
 );
 
+--Insert values into respective tables.
 insert into salesman values (&salesman_id, '&name', '&city', &commission);
 
 insert into customer values (&customer_id, '&customer_name', '&city', &grade, &salesman_id);
 
-update customer
-set city='bangalore', salesman_id = 1000
-where customer_id = 50;
-
 insert into orders values (&ord_no, &purchase_amt, '&ord_date', &customer_id, &salesman_id);
 
+-- 1. Count the customers with grades above Bangalore’s average.
 select COUNT(c.customer_id)
 from customer c
 where c.grade > (select AVG(grade) from customer where city='bangalore');
 
+-- 2. Find the name and numbers of all salesmen who had more than one customer.
 select s.name, s.salesman_id
 from salesman s, customer c
 where s.salesman_id = c.salesman_id
 group by s.name, s.salesman_id
 having count(customer_id) > 1;
 
+-- 3. List all salesmen and indicate those who have and don’t have customers in their cities. (Use UNION operation)
 (select s.name, s.salesman_id, c.customer_name
 from salesman s, customer c
 where s.salesman_id = c.salesman_id and s.city=c.city)
@@ -59,12 +62,13 @@ union
 	where s1.salesman_id = c1.salesman_id and s1.city != c1.city
 );
 
+-- 4. Create a view that finds the salesman who has the customer with the highest order of a day.
 create view max_ord as
 	select s.salesman_id, s.name, c.customer_id, c.customer_name, o.ord_date, o.purchase_amt
 	from salesman s, customer c, orders o
 	where s.salesman_id = c.salesman_id and c.customer_id = o.customer_id;
-
 select * from max_ord m
 where m.purchase_amt = (select max(m1.purchase_amt) from max_ord m1 where m.ord_date = m1.ord_date);
 
+-- 5. Demonstrate the DELETE operation by removing salesman with id 1000. All his orders must also be deleted.
 delete from salesman where salesman_id = 1000;
